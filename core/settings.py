c5d898a1cb1115a4481f77616475d0d32b4fefe0
Giovanni <mail@giovanni-witulski.com>
2026-04-24 03:42:18 +0200

    chore: Added suggestions and bot_control to Installed apps

--- core/settings.py
+++ core/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps/suggestions',
-    'apps/bot_config',
+    'apps.suggestions',
+    'apps.bot_config',
 ]
 
 MIDDLEWARE = [
